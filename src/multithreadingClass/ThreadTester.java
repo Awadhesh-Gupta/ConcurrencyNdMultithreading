45ee38d728e58952afede393c43bd206dfe4c8e1
Awadhesh-Gupta <22054295@kiit.ac.in>
2025-05-30 10:36:48 +0530

    implementing run() method and executing

---
+++ src/multithreadingClass/ThreadTester.java
@@ -0,0 +1,10 @@
+package multithreadingClass;
+public class ThreadTester {
+    public static void main(String[] args) {
+        System.out.println("Main is starting");
+        Thread thread1 = new Thread1();
+        thread1.start();
+
+        System.out.println("Main is exiting");
+    }
+}
